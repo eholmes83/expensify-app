312e70f12cf69393120a03bb137f49f24741caf4
Ed Holmes <eholmes83@yahoo.com>
2023-01-12 17:14:04 -0500

    removing help page and adding public routing

--- src/components/Help.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HelpPage = () => (
-  <div>
-    Having problems? Reach out with any issues to help@gmail.com
-  </div>
-)
-
-export default HelpPage
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { Router, Route, Switch } from 'react-router-dom'
 import createHistory from 'history/createBrowserHistory'
 import AddExpensePage from '../components/AddExpense'
-import HelpPage from '../components/Help'
 import ExpenseDashboardPage from '../components/ExpenseDashboard'
 import EditExpensePage from '../components/EditExpense'
 import NotFoundPage from '../components/NotFound'
 import Login from '../components/Login'
 import PrivateRoute from './PrivateRoute'
+import PublicRoute from './PublicRoute'
 
 export const history = createHistory()
 
@@ -15,11 +15,10 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <Route path="/" component={Login} exact={true} />
+        <PublicRoute path="/" component={Login} exact={true} />
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
         <PrivateRoute path="/create" component={AddExpensePage} />
         <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
